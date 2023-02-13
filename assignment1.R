628cc26bb401bd8f8187c89f12bae192b8c64e78
“Graham <graham.l.hurley@gmail.com>
2023-02-13 21:01:05 +0000

    Fourth commit with comments.

--- assignment1.R
+++ assignment1.R
@@ -19,18 +19,21 @@ View(housesales)
 # USE COMMENTS (PREFACED BY THE #) TO ACCOMPANY YOUR CODE TO EXPLAIN WHAT YOU'RE DOING 
 # FOR EACH STEP AS NEEDED
 
-
+# I could not figure out how to insert quarto into the code here. I installed tidyverse multiple times and it
+# did not work.  
 
 # 1) The column "year" indicates the year. Filter to return only results from 2015. 
 # (note that because the column is numeric, you wouldn't use quotes as with text when setting your filter value)
 
 filter(housesales, year == 2015)
-
+# I used the filter command to only filter if the year is 2015.
 
 # 2) Similar to the above question, filter results from 2010 onward, so 2010-2015.
 
 
 filter(housesales, Year >= 2010 & Year <= 2015)
+
+# I used the filter command to filter results betweeen 2010 and 2015 for the dataset. 
 
 
 # 3) The "city" column contains the relevant city. Filter to return only results for Houston
@@ -38,13 +41,14 @@ filter(housesales, Year >= 2010 & Year <= 2015)
 
 filter(housesales,city == "Houston")
 
-
+# I used the filter command to filter results only for Houston. 
 
 # 4) Filter for only where the city is Houston as above, and now also filter for only 
 # the year 2010. The results should give you 12 records, one for each month in 2010 for Houston.
 
 filter(housesales,city == "Houston",year == 2010)
 
+# I used the filter command to filter results only for Houston. 
 
 
 # 5) Build on what you've done above. Filter for only where the city is Houston as above, 
@@ -55,13 +59,17 @@ filter(housesales,city == "Houston",year == 2010)
 
 filter(housesales,city == "Houston",year == 2010,arrange(desc(sales)))
 
+# This one was tricky, the arrange command is kind of wonky and whenever I run this command above it does not work. 
+# Not sure why because the code follows the examples from class. 
+
+
 
 # 6) Piggyback on what you've done above. 
 # Do the same as #5, only this time instead of Houston return records for Dallas
 
 filter(housesales,city == "Dallas",year == 2010,arrange(desc(sales)))
 
-
+# I followed the same formula from above but changed the name in quotes to Dallas
 
 
 # 7) Sometimes metrics like home sales have a seasonal quality to them, much like 
@@ -75,7 +83,7 @@ filter(housesales,city == "Dallas",year == 2010,arrange(desc(sales)))
 
 filter(housesales, city == "Dallas", month == 6)
 
-
+# I filtered housesales by month and city. 
 
 # 8) Build on what we've done above. Now that we have June numbers for Dallas for every year,
 # arrange the results by sales from highest-to-lowest to show what year had the highest June sales.
@@ -83,6 +91,7 @@ filter(housesales, city == "Dallas", month == 6)
 
 filter(housesales, city == "Dallas", month == 6, arrange(desc(sales)))
 
+# I filtered housesales by month and city. I also used the same arrange code as before but it still did not work.  
 
 
 # 9) Now do the same as Question 8, but this time instead of Dallas, show results for Corpus Christi
@@ -90,11 +99,13 @@ filter(housesales, city == "Dallas", month == 6, arrange(desc(sales)))
 filter(housesales, city == "Corpus Christi", month == 6, arrange(desc(sales)))
 
 
-
+# I changed Dallas to Corpus Christi. 
 
 
 # 10) Finally, using the Corpus Christi code from Question 9, instead of June
 # use the month of February (2) instead.
 
 filter(housesales, city == "Corpus Christi", month == 2, arrange(desc(sales)))
+
+#I changed the month to 2 as opposed to 6. 
 
